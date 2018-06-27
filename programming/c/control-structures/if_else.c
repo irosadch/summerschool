b8d081965cfa35be0cf190b8403c95b238b513bb
irosadch <irina.osadchuk8@gmail.com>
2018-06-27 11:51:39 +0300

    work

--- programming/c/control-structures/if_else.c
+++ programming/c/control-structures/if_else.c
@@ -3,7 +3,25 @@
 int main(int argc, char *argv[])
 {
     int i;
+printf("Enter number!\n");
+scanf("%d",&i);
 
+switch (i){
+	case 0:
+	printf("zero\n");
+	break;
+	case 1:
+	 printf("one\n");
+	break;
+default:
+ if (i>100) {
+printf("Many!!!\n");
+} else {
+printf("%d\n",i);
+}
+break;
+
+}
     // write the control structure as described in the exercise
     // and run the code with different values for i
 
